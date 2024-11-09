ab1b689469d887bdc29eb8b1fec81fc9ab0340ff
Helio Silva <suporte.computek@gmail.com>
2024-11-09 11:14:55 -0300

    Commit inicial

---
+++ src/main/kotlin/com/computek/painel/PainelApplication.kt
@@ -0,0 +1,11 @@
+package com.computek.painel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PainelApplication
+
+fun main(args: Array<String>) {
+	runApplication<PainelApplication>(*args)
+}
